a9f7fed655653f4e60ab0c1319bb61ddfbe69909
EC2 Default User <ec2-user@ip-172-31-61-171.ec2.internal>
2020-12-09 04:37:27 +0000

    ログイン機能、画像投稿昨日

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :new, :create, :destroy]
+  resources :images
   get 'top/main'
   post 'top/login'
   root 'top#main'
   get 'top/logout'
+  get 'get_image/:id', to: 'images#get_image'
 #    get "application/L4"
 #    root "application#L4"
 end
